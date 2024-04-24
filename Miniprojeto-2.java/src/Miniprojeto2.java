3e06a670325085a76d1ee3b04afe92c2196426a7
MClaraFerreira5 <mariaferreira578.9cl@gmail.com>
2024-04-23 22:21:48 -0300

    Miniprojeto2_Final

--- Miniprojeto-2.java/src/Miniprojeto2.java
+++ Miniprojeto-2.java/src/Miniprojeto2.java
@@ -4,34 +4,70 @@ public class Miniprojeto2 {
         Scanner y = new Scanner(System.in);
         System.out.println("Digite o nome do seu personagem: ");
         String user = y.nextLine();
-        Personagemm nome = new Personagemm(user, 100);
+        Personagemm nome = new Personagemm(user, 20);
+        
         System.out.println("Pronto!! \nSeu personagem se chama " + nome.nome + ", e ele tem " + nome.energia + " de energia." );
+        
         System.out.println("Agora crie o seu segundo personagem");
         String user2 = y.nextLine();
-        Personagemm nome2 = new Personagemm(user2, 100);
+        
+        Personagemm nome2 = new Personagemm(user2, 20);
+        
         System.out.println("Pronto!! \nSeu segundo personagem se chama " + nome2.nome + ", e ele tem " + nome2.energia + " de energia." );
-        System.out.println("Agora eles vão lutar entre si");
-        System.out.println("Você quer seguir o " + nome.nome + "ou" + nome2.nome + "?");
+        System.out.println("\u001B[33mO labirinto\u001B[0m");
+        System.out.println(nome.nome + " e " + nome2.nome + " estão em um labirinto no meio de uma floresta sem comida ou água e " +
+        "eles devem sair rápido ou irão morrer \nEles começam a seguir em frente até que encontram uma fruta misteriosa. \n" +
+        "Escolha se os personagens irão comer a fruta. Digite sim ou nao." );
+        
         String per = y.nextLine();
-        if (per.equals(nome.nome)){
-            System.out.println("Agora você deseja atacar ou defender? ");
-            String atacdef = y.nextLine();
-            if (per.equals("atacar")){
+        String pa = nome.nome + " e " + nome2.nome;
+        
+        if (per.equals("sim")){
+                nome.diminuirEnergia();
+                nome2.diminuirEnergia();
+                System.out.println("A fruta que eles comeram era venenosa e " + nome.nome + " e " + nome2.nome + "passaram mal");
+                System.out.println("Após eles serem envenenados " + pa + " ficam desesperados em busca de uma saida eles avistam 2" + 
+                " caminhos um com mata fechada e outro com um campo aberto");
+                System.out.println("Digite 1 para seguir o da mata fechada e 2 para o do campo aberto");
+                String mata_campo = y.nextLine();
                 
+                if(mata_campo.equals("1")){
+                    System.out.println("Eles seguem pela mata fechada e vão ficando cada vez mais perdidos na densa vegetação" + 
+                    "e ficam completamente esgotados ");
+                    nome.diminuirEnergia();
+                    nome2.diminuirEnergia();
+            
+                }
+                else if(mata_campo.equals("2")){
+                    System.out.println("No campo eles conseguem ver com clareza o fim do labirinto e apesar de estaram muito" + 
+                    " cansados eles conseguem achar o \u001B[32mfim do labirinto\u001B[0m");
+                }           
             }
-            else if(per.equals("defesa")){
-
+        
+        
+        else if(per.equals("nao")){
+            System.out.println("Eles passam reto pela fruta e seguem em direção ao meio do labirinto, até que eles avistam um animal" +
+            " ao chegar perto o animal começa a andar em uma direção do labirinto. \nEntão eles pensam se deveriam seguir-lo ou não");
+            System.out.println("Digite se você deseja seguir-lo ou não. Digite sim ou nao");
+            String segue_ou_n = y.nextLine();
+           
+            if(segue_ou_n.equals("sim")){
+                System.out.println("Eles decidem seguir o animal e ele os leva a um campo onde eles conseguem ver a " + 
+                "\u001B[32msaída do labirinto!!\u001B[0m");
             }
+            
+            else if(segue_ou_n.equals("nao")){
+                System.out.println("Eles decidem ir para outro caminho, após muita caminhada eles não encontram a saída e ficam" + 
+                " mais e eles perdem energia");
+                nome.diminuirEnergia();
+                nome2.diminuirEnergia();
+                System.out.println("Após muito tempo e por estarem esgotados eles ficam sem suprimentos.");
+                nome.diminuirEnergia();
+                nome2.diminuirEnergia();
+            }
+            }
+        y.close();
         }
-        else if(per.equals(nome2.nome)){
-            System.out.println("Agora você deseja atacar ou defender? ");
-            String atacdef2 = y.nextLine();
-            if(per.equals("atacar")){
 
-            }
-            else if(per.equals("defesa")){
-
-            }
-        }
-    }
 }
+
--- Miniprojeto-2.java/src/Personagemm.java
+++ Miniprojeto-2.java/src/Personagemm.java
@@ -5,13 +5,18 @@ public class Personagemm {
         this.nome = nome;
         this.energia = energia;
         }
-
-    public int perder(int energia){
-        energia = energia - 50;
-        return energia;
-        }
-    public int igual(int energia){
-        return energia;
+    public void diminuirEnergia() {
+        this.energia -= 17;
+            if (this.energia <= 0){
+                System.out.println(" \u001B[31mToda a energia foi esgotada. O personagem morreu\u001B[0m");
+            }
+            else{
+                System.out.println("Foram perdidos \u001B[31m17 pontos de energia\u001B[0m");
+                System.err.println("Agora a energia de " + this.nome + " é: " + this.getEnergia());
             }
         }
-    
+
+    public int getEnergia() {
+        return this.energia;
+        }
+    }
